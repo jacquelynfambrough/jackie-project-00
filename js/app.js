8202eeb5609de782ab67232cb82192451d086ebe
YOUR_GITHUB_USERNAME <jacquelynfambrough@gmail.com>
2016-03-26 15:33:05 -0700

    playing with keycodes and event listeners to assign keys to a function

---
+++ js/app.js
@@ -0,0 +1,18 @@
+console.log("JS is on");
+$(document).on("ready", function(){
+
+  var playerOne = $('#p-1');
+  var playerTwo = $('#p-2');
+
+
+  $(window).on("keypress", function eventHandler(event){
+if (event.keyCode === ){
+  console.log("spacebar");
+
+    }
+
+
+ });
+
+
+});
