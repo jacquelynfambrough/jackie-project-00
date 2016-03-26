1855e5a6538cdda031c700a8dfc02a67942f9d76
YOUR_GITHUB_USERNAME <jacquelynfambrough@gmail.com>
2016-03-26 16:17:40 -0700

    changed event listener to keypress rather than keydown

--- js/app.js
+++ js/app.js
@@ -3,16 +3,19 @@ $(document).on("ready", function(){
 
   var playerOne = $('#p-1');
   var playerTwo = $('#p-2');
+  var pOneKey = 97;
+  var pTwoKey = 108;
 
 
-  $(window).on("keydown", function eventHandler(event){
-    if (event.keyCode === 65){
+  $(window).on("keypress" , function eventHandler(e){
+    if (e.which === pOneKey){
       console.log("A");
     }
   });
 
-  $(window).on("keydown", function eventHandler(event){
-    if (event.keyCode === 76){
+  $(window).on("keypress" , function eventHandler(e){
+    // console.log(e);
+    if (e.which === pTwoKey){
       console.log("L");
     }
   });
